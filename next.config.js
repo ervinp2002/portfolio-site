a39f2ee04ff30775161498e7fa00cb1ca6e29472
Ervin Pangilinan <eepang1@gmail.com>
2023-02-13 12:30:46 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['media.licdn.com', 'avatars.githubusercontent.com', 'github.com', 'drive.google.com', 'raw.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
