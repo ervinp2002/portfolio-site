342032c26a8e7354980b2f0ffe3ceac3c292c566
Ervin Pangilinan <eepang1@gmail.com>
2023-02-13 12:31:53 -0500

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,25 @@ import Head from "next/head";
 import { NextPage } from "next";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-gray-600 text-white h-screen">
+    <div className="bg-neutral-800 text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Ervin's Portfolio</title>
       </Head>
 
       <Header />
       {/*Hero*/}
-      <section id="hero">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/*About*/}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/*Experience*/}
 
