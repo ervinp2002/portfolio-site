09a740f4d11963fbbfe947cac14fab92c1b2405b
Ervin Pangilinan <eepang1@gmail.com>
2023-02-13 12:31:20 -0500

    Add files via upload

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
 import React, { ReactElement } from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {}
 
@@ -10,11 +13,39 @@ export function Hero({}: Props): ReactElement {
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="gold" />
-      </h1>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden z-0">
+      <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        alt="Personal Photo"
+        src="https://avatars.githubusercontent.com/u/33070329?v=4"
+        width={256}
+        height={256}
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Undergraduate Student
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="gold" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
