95ad26f4592ee80ef4bf75d13ce3116d1c3c9696
Ervin Pangilinan <eepang1@gmail.com>
2023-02-14 11:56:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media.licdn.com', 'avatars.githubusercontent.com', 'github.com', 'drive.google.com', 'raw.githubusercontent.com']
+    domains: ['media.licdn.com', 'avatars.githubusercontent.com', 'github.com', 'drive.google.com', 
+    'raw.githubusercontent.com', 'w7.pngwing.com', 'upload.wikimedia.org', 'cdn.pixabay.com', 'e7.pngegg.com']
   }
 }
 
