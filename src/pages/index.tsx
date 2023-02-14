b14ef86083ac7aaf60f1d3ccf969203436106f8f
Ervin Pangilinan <eepang1@gmail.com>
2023-02-14 11:57:20 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { NextPage } from "next";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -12,24 +15,28 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      {/*Hero*/}
+
       <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/*About*/}
       <section id="about" className="snap-center">
         <About />
       </section>
 
-      {/*Experience*/}
+      <section id="experience" className="snap-center">
+        <Experience />
+      </section>
 
-      {/*Skills*/}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
-      {/*Projects*/}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/*Contact Me*/}
-
     </div>
   );
 };
