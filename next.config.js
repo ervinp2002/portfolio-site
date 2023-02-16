e5811225711e26c5fc60f124edfda5e98f478866
Ervin Pangilinan <eepang1@gmail.com>
2023-02-16 13:46:37 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['media.licdn.com', 'avatars.githubusercontent.com', 'github.com', 'drive.google.com', 
-    'raw.githubusercontent.com', 'w7.pngwing.com', 'upload.wikimedia.org', 'cdn.pixabay.com', 'e7.pngegg.com']
+    'raw.githubusercontent.com', 'w7.pngwing.com', 'upload.wikimedia.org', 'cdn.pixabay.com', 'e7.pngegg.com',
+    'cdn-icons-png.flaticon.com']
   }
 }
 
