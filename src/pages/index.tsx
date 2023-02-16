be3b9cb22797fb0a27ea9a55509b5bada0891a13
Ervin Pangilinan <eepang1@gmail.com>
2023-02-16 13:34:22 -0500

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,38 +6,69 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
+import ClientOnly from "@/components/ClientOnly";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-neutral-800 text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
-      <Head>
-        <title>Ervin's Portfolio</title>
-      </Head>
+    <ClientOnly>
+      <div
+        className="bg-neutral-800 text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden 
+    z-0 scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin"
+      >
+        <Head>
+          <title>Ervin's Portfolio</title>
+        </Head>
 
-      <Header />
+        <Header />
 
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
+        <section id="hero" className="snap-start">
+          <Hero />
+        </section>
 
-      <section id="about" className="snap-center">
-        <About />
-      </section>
+        <section id="about" className="snap-center">
+          <About />
+        </section>
 
-      <section id="experience" className="snap-center">
-        <Experience />
-      </section>
+        <section id="experience" className="snap-center">
+          <Experience />
+        </section>
 
-      <section id="skills" className="snap-start">
-        <Skills />
-      </section>
+        <section id="skills" className="snap-start">
+          <Skills />
+        </section>
 
-      <section id="projects" className="snap-start">
-        <Projects />
-      </section>
+        <section id="projects" className="snap-start">
+          <Projects />
+        </section>
 
-      {/*Contact Me*/}
-    </div>
+        <section id="contact" className="snap-start">
+          <ContactMe />
+        </section>
+
+        <Link href="#hero">
+          <footer className="sticky bottom-5 w-full cursor-pointer">
+            <div className="flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-10 h-10 rounded-full filter grayscale hover:grayscale-0"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+            </div>
+          </footer>
+        </Link>
+      </div>
+    </ClientOnly>
   );
 };
 
