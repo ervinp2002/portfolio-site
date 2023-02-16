d40924834ba9422a16d5bf19c72ffd8c0fd623c3
Ervin Pangilinan <eepang1@gmail.com>
2023-02-16 13:36:46 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}", 
-    "./src/components/**/*.{js,ts,jsx,tsx}>"],
+    "./src/components/**/*.{js,ts,jsx,tsx}"],
     
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }
